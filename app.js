982d57058b402e31a32cc7fb5a665e4aba7e8525
souviksarkhel <souviksarkhel.kgp@gmail.com>
2022-01-18 16:10:59 +0530

    Database Structure Changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const multer  = require('multer');
+const multer = require('multer');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, './uploads/');
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     cb(null, file.originalname + '-' + Date.now() + '.jpeg'); //Appending extension
   }
 });
@@ -37,7 +37,9 @@ const upload = multer({
 
 app.set('view engine', 'ejs');
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.use(session({
   secret: "Our little secret.",
@@ -62,17 +64,28 @@ mongoose.connect("mongodb://localhost:27017/jolubookDB", {
 });
 const userSchema = mongoose.Schema({
   name: String,
-  year:String,
-  department: String,
-  currorg:String,
   username: String,
   password: String,
   dp:String
 });
+const personSchema = mongoose.Schema({
+  name: String,
+  mobile: String,
+  year: String,
+  department: String,
+  currorg: String,
+  prevorg: String,
+  country: String,
+  currcity: String,
+  hometown: String,
+  username: String,
+  dp: String
+});
 const postSchema = mongoose.Schema({
-  postedtext:String,
-  time:String,
-  postedby:Object
+  postedtext: String,
+  time: String,
+  postedby: String,
+  creatorusername: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -81,6 +94,7 @@ userSchema.plugin(findOrCreate);
 
 
 const User = mongoose.model("User", userSchema);
+const Person = mongoose.model("Person", personSchema);
 const Post = mongoose.model("Post", postSchema);
 passport.use(User.createStrategy());
 
@@ -94,157 +108,272 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-app.get("/logout", function(req, res){
+app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
 
 app.get("/account", function(req, res) {
-  if (req.isAuthenticated()){
-    //console.log(req.user);
-    res.render("account",{user:req.user});
+  if (req.isAuthenticated()) {
+    Person.findOne({
+      'username': req.user.username
+    }, function(error, fu) {
+      if(error) console.log(error);
+      else{
+        res.render("account", {
+          user: req.user,accountholder:fu
+        });
+      }
+    });
+
 
   } else {
     res.redirect("/login");
   }
 });
 app.get("/editaccount", function(req, res) {
-  if (req.isAuthenticated()){
-    //console.log(req.user);
-    res.render("accountedit",{user:req.user});
-
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.post("/editaccount",upload.single('newImg') ,function(req, res) {
-console.log(req.file);
-  if (req.isAuthenticated()){
-    //console.log(req.user);
-    User.updateOne({
-              username: req.body.userEmail
-            },{
-              $set: {
-                currorg:req.body.currPosition,
-                dp:req.file.filename
-              }
-            }, {
-              upsert: true
-            }, function(err) {
-              if (err) console.log(err);
-              else console.log("User Details Updated");
-            })
-    req.user.currorg=req.body.currPosition;
-    req.user.dp=req.file.filename;
-
-
-
-
-
-    res.render("account",{user:req.user});
-
-  } else {
-    res.redirect("/login");
-  }
-
-
-
-});
-
-
-
-
-
-
-app.get("/feed", function(req, res){
-  if (req.isAuthenticated()){
-
-    Post.find({"postedtext": {$ne:''}}, function(err, foundPosts){
-      if (err){
-        console.log(err);
-      } else {
-        if (foundPosts) {
-          //console.log(foundPosts);
-          res.render("feed",{allposts: foundPosts.reverse(),currentuser:req.user});
-        }
-      }
-    });
-
-
-  } else {
-    res.redirect("/login");
-  }
-});
-
-
-app.post("/login",function(req,res){
-
-  const user = new User({
-  username: req.body.username,
-  password: req.body.password
-});
-
-req.login(user, function(err){
-  if (err) {
-    console.log(err);
-  } else {
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/feed");
-    });
-  }
-});
-});
-
-
-
-
-app.post("/register",function(req,res){
-
-  User.register({name: req.body.name,
-  year: req.body.year,
-  department: req.body.department,
-  currorg:req.body.currorg,
-  username: req.body.username,dp:"avater.png"}, req.body.password, function(err, user){
-      if (err) {
-        console.log(err);
-        res.redirect("/register");
-      } else {
-        passport.authenticate("local")(req, res, function(){
-          res.redirect("/feed");
+  if (req.isAuthenticated()) {
+    Person.findOne({
+      'username': req.user.username
+    }, function(error, fu) {
+      if(error) console.log(error);
+      else{
+        res.render("accountedit", {
+          user: req.user,accountholder:fu
         });
       }
     });
 
+
+  } else {
+    res.redirect("/login");
+  }
 });
 
-app.post("/addpost", function(req, res){
-  if (req.isAuthenticated()){
-  var currdate = new Date();
-  var datetime =  currdate.toLocaleDateString()+" "+currdate.toLocaleTimeString();
-    if(req.body.inputText!=='')
+app.post("/editaccount", upload.single('newImg'), function(req, res) {
+  if (req.isAuthenticated()) {
+    Person.updateOne({
+      username: req.body.username
+    }, {
+      $set: {
+        mobile: req.body.mobile,
+        currcity: req.body.currcity,
+        currorg: req.body.currorg,
+        country: req.body.country,
+        hometown: req.body.hometown,
+        prevorg: req.body.prevorg
+      }
+    }, {
+      upsert: true
+    }, function(err) {
+      if (err) console.log(err);
+      else console.log("User Details Updated");
+    })
+    if(req.file)
     {
-      User.findById(req.user.id, function(err, foundUser){
+      Person.updateOne({
+        username: req.body.username
+      }, {
+        $set: {
+          dp: req.file.filename
+        }
+      }, {
+        upsert: true
+      }, function(err) {
+        if (err) console.log(err);
+      })
+      User.updateOne({
+        username: req.body.username
+      }, {
+        $set: {
+          dp: req.file.filename
+        }
+      }, {
+        upsert: true
+      }, function(err) {
+        if (err) console.log(err);
+      })
+      req.user.dp = req.file.filename;
+
+    }
+    //dp: req.file.filename
+    //req.user.currorg = req.body.currPosition;
+    //
+
+
+
+
+    res.redirect("/account");
+
+  } else {
+    res.redirect("/login");
+  }
+
+
+
+});
+
+
+
+
+
+
+app.get("/feed", function(req, res) {
+  if (req.isAuthenticated()) {
+
+    Post.find({
+    }, function(err, foundPosts) {
+      if (err) {
+        console.log(err);
+      } else {
+        //console.log(foundPosts);
+        if (foundPosts) {
+          var articles = [];
+          for (var i = 0; i < foundPosts.length; i++) {
+            let obj = foundPosts[i];
+            Person.findOne({
+              'username': obj.creatorusername
+            }, function(error, fu) {
+              if (error) console.log(error);
+              else {
+                /*console.log(obj);
+                console.log(fu);
+                console.log(obj);
+                console.log("--------------------");*/
+
+
+                var post = {
+                  author: obj.postedby,
+                  post: obj.postedtext,
+                  authordp: fu.dp,
+                  posttime: obj.time,
+                  authorg: fu.currorg,
+                  authcity: fu.currcity,
+                  authcountry:fu.country,
+                  authpassyear:fu.year,
+                  authdept:fu.department
+                };
+                articles.push(post);
+                if(articles.length===foundPosts.length) res.render("feed", {
+                  allposts: articles.reverse(),
+                  currentuser: req.user
+                });;
+              }
+            });
+          }
+          //console.log(foundPosts);
+
+        }
+      }
+    });
+
+
+  } else {
+    res.redirect("/login");
+  }
+});
+
+
+
+
+
+app.post("/login", function(req, res) {
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/feed");
+      });
+    }
+  });
+});
+
+
+
+
+app.post("/register", function(req, res) {
+
+  User.register({
+    name: req.body.name,
+    username: req.body.username,dp: "avater.jpeg"
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      var newPerson = new Person({
+        name: req.body.name,
+        year: req.body.year,
+        department: req.body.department,
+        currorg: req.body.currorg,
+        username: req.body.username,
+        mobile: "",
+        prevorg: "",
+        country: "",
+        currcity: "",
+        hometown: "",
+        dp: "avater.jpeg"
+      })
+      newPerson.save(function(err) {
         if (err) {
           console.log(err);
         } else {
-          if (foundUser) {
-            var newPost = new Post({
-                      postedtext: req.body.inputText,
-                      postedby:foundUser,
-                      time: datetime
-                    })
-            newPost.save(function(err) {
-                      if (err) {
-                        console.log(err);
-                      } else {
 
-                      }
-                    });
-          }
         }
       });
 
+
+
+
+
+
+
+
+
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/feed");
+      });
+    }
+  });
+
+});
+
+app.post("/addpost", function(req, res) {
+  if (req.isAuthenticated()) {
+    const currdate = new Date();
+    const formattedDate = currdate.toLocaleDateString('en-GB', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }).replace(/ /g, '/');
+    const formattime = currdate.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+
+
+    var datetime = formattedDate + " " + formattime;
+    if (req.body.inputText !== '') {
+      var newPost = new Post({
+        postedtext: req.body.inputText,
+        postedby: req.user.name,
+        time: datetime,
+        creatorusername: req.user.username
+      })
+      newPost.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+
+        }
+      });
     }
     res.redirect("/feed");
   } else {
@@ -255,7 +384,7 @@ app.post("/addpost", function(req, res){
 
 
 app.get("/", function(req, res) {
-  if (req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     res.redirect("/feed");
   } else {
     res.render("homepage");
@@ -263,14 +392,14 @@ app.get("/", function(req, res) {
 });
 
 
-app.get('/register', function(req, res){
-    res.render('register');
+app.get('/register', function(req, res) {
+  res.render('register');
 });
 
-app.get('/login', function(req, res){
-    res.render('login');
+app.get('/login', function(req, res) {
+  res.render('login');
 });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
